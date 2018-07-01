e7fffa06108be56af18e89c69c0a0ab7036ec460
Ivan Oreskovic <ivan.oreskovic@gmail.com>
2018-07-01 12:35:51 +0200

    Problem(s) solved

--- src/main/java/com/rimac/MyFirstFizzBuzz.java
+++ src/main/java/com/rimac/MyFirstFizzBuzz.java
@@ -1,6 +1,8 @@
 package com.rimac;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class MyFirstFizzBuzz implements FizzBuzz {
     /*
@@ -15,6 +17,25 @@ public class MyFirstFizzBuzz implements FizzBuzz {
     */
     @Override
     public List<String> doMagic(int from, int until) {
-        throw new UnsupportedOperationException("Not yet implemented!");
+        return IntStream.rangeClosed(from, until)
+                .mapToObj(this::abracadabra)
+                .collect(Collectors.toList());
+    }
+
+    protected final String abracadabra(int i) {
+        final StringBuilder sb = new StringBuilder();
+
+        if (i % 3 == 0) {
+            sb.append("Fizz");
+        }
+        if (i % 5 == 0) {
+            sb.append("Buzz");
+        }
+
+        if (sb.length() == 0) {
+            sb.append(i);
+        }
+
+        return sb.toString();
     }
 }
